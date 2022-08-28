5e7f07e2229b4069e672c9e4d0ed53ef44ff7073
followthwhiterabbit <kaankaraalioglu@yahoo.com>
2022-08-28 12:55:04 +0200

    core dump problem && editing log file

---
+++ build/CMakeFiles/grepex.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grepex.dir/grep.cpp.o"
+  "CMakeFiles/grepex.dir/main.cpp.o"
+  "grepex"
+  "grepex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grepex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
